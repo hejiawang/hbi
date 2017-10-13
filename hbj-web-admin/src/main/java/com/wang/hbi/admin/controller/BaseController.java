60b0a0010b000ea45d6c546c401b3f67d9f249b4
hejiawang <952327407@qq.com>
2017-10-13 22:51:56 +0800

    layui

---
+++ hbj-web-admin/src/main/java/com/wang/hbi/admin/controller/BaseController.java
@@ -0,0 +1,31 @@
+package com.wang.hbi.admin.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * base controller
+ * @author HeJiawang
+ * @date   20171013
+ */
+public abstract class BaseController {
+
+    protected final Logger loger = LoggerFactory.getLogger( this.getClass() );
+
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+    protected HttpSession session;
+
+    @ModelAttribute
+    public void setReqAndRes(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+        this.session = request.getSession();
+    }
+}
---
+++ hbj-web-admin/src/main/java/com/wang/hbi/admin/controller/index/Index.java
@@ -0,0 +1,53 @@
+package com.wang.hbi.admin.controller.index;
+
+import com.wang.hbi.admin.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Enumeration;
+
+/**
+ *
+ *
+ * @author HeJiawang
+ * @date   20171013
+ */
+@Controller
+@RequestMapping("/")
+public class Index extends BaseController {
+
+    /**
+     * 首页页面请求
+     * @return 登录页
+     */
+    @RequestMapping(value = "", method= RequestMethod.GET)
+    public String index(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/main", method= RequestMethod.GET)
+    public String main(){
+        return "main";
+    }
+
+    /**
+     * 模板页面请求
+     * @param module
+     * @param view
+     * @return
+     */
+    @RequestMapping(value = "/{project}/{module}/{view}.html", method=RequestMethod.GET)
+    public String busView (@PathVariable String project, @PathVariable String module, @PathVariable String view, Model model) {
+        Enumeration<String> names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            String value = request.getParameter(name);
+            model.addAttribute(name, value);
+        }
+
+        return new StringBuilder(project).append("/").append(module).append("/").append(view).toString();
+    }
+}
