50bb8363f26882672fa9c89907249db724f136b3
赫家旺 <952327407@qq.com>
2017-11-01 10:56:06 +0800

    convert

--- hbj-web-admin/src/main/java/com/wang/hbi/admin/controller/BaseController.java
+++ hbj-web-admin/src/main/java/com/wang/hbi/admin/controller/BaseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * base controller
@@ -27,5 +30,87 @@ public abstract class BaseController {
         this.request = request;
         this.response = response;
         this.session = request.getSession();
+    }
+
+    /**
+     * 集合转换
+     * @param sources sources
+     * @param targetClass targetClass
+     * @param <E> object
+     * @return object
+     * @throws Exception exception
+     */
+    protected <E> List<E> convertList(List<Object> sources, Class<E> targetClass) throws Exception {
+        List<E> targetList = new ArrayList<>();
+        for (Object source : sources) {
+            targetList.add(convert(source, targetClass));
+        }
+        return targetList;
+    }
+
+    /**
+     * 对象转换
+     * @param source source
+     * @param clazz clazz
+     * @param <T> object
+     * @return object
+     */
+    protected <T> T convert(Object source, Class<T> clazz) {
+        T target = null;
+        try {
+            target = clazz.newInstance();
+            for (Class<?> c = source.getClass(); !Object.class.equals(c); c = c.getSuperclass()) {
+                for (Field sourceField : c.getDeclaredFields()) {
+                    Class<?> targetClass = clazz;
+                    sourceField.setAccessible(true);
+                    for (Object o = new Object(); o != null;) {
+                        try {
+                            o = null;
+                            Field targetField = targetClass.getDeclaredField(sourceField.getName());
+                            targetField.setAccessible(true);
+                            if (sourceField.getType().isEnum() && targetField.getType().isEnum()) {
+                                Object sourceEnumObject = sourceField.get(source);
+                                if (sourceEnumObject != null) {
+                                    Object convertEnum = convertEnum(sourceEnumObject, targetField.getType());
+                                    targetField.set(target, convertEnum);
+                                }
+                            } else {
+                                Object sourceObject = sourceField.get(source);
+                                if (sourceObject != null) {
+                                    targetField.set(target, sourceObject);
+                                }
+                            }
+                        } catch (NoSuchFieldException e) {
+                            o = e;
+                            targetClass = targetClass.getSuperclass();
+                            if (Object.class.equals(targetClass)) {
+                                o = null;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return target;
+    }
+
+    /**
+     * 枚举转换
+     * @param sourceEnumObject sourceEnumObject
+     * @param targetEnumClass targetEnumClass
+     * @return Object
+     * @throws Exception exception
+     */
+    protected Object convertEnum(Object sourceEnumObject, Class<?> targetEnumClass) throws Exception {
+        if (sourceEnumObject == null) return null;
+
+        String sourceEnumValue = sourceEnumObject.toString();
+        try {
+            Field targetEnumField = targetEnumClass.getField(sourceEnumValue);
+            return targetEnumField.get(null);
+        } catch (NoSuchFieldException e) {}
+        return null;
     }
 }
