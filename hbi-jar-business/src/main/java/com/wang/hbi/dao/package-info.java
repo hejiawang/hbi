28a5f42bc28ed573210f2e559916e3cb623c99d9
赫家旺 <952327407@qq.com>
2017-11-19 14:11:04 +0800

    modify configure file

---
+++ hbi-jar-business/src/main/java/com/wang/hbi/dao/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author HeJiawang
+ * @Date 2017/11/19 13:53
+ */
+package com.wang.hbi.dao;
---
+++ hbi-jar-business/src/main/java/com/wang/hbi/service/impl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author HeJiawang
+ * @Date 2017/11/19 13:54
+ */
+package com.wang.hbi.service.impl;
